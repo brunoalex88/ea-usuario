0624a3fcee0c66d74bc5f8fb383ecde99694351e
Bruno Alex de Oliveira <brunoalex_88@yahoo.com.br>
2022-07-19 20:01:10 -0300

    Commit inicial do projeto com as dependências

---
+++ src/main/java/com/estacioneaqui/eausuario/EaUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.estacioneaqui.eausuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estacioneaqui/eausuario/EaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estacioneaqui.eausuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
