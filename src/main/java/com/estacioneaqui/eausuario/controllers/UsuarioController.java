a98fc326c5d7df1a370ddd910e8737b7128b2dfc
Bruno Alex de Oliveira <brunoalex_88@yahoo.com.br>
2022-07-31 12:26:57 -0300

    Alteração de usuário #2

--- src/main/java/com/estacioneaqui/eausuario/controllers/UsuarioController.java
+++ src/main/java/com/estacioneaqui/eausuario/controllers/UsuarioController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -21,7 +23,7 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping("/registrar")
-    public ResponseEntity<Object> registrarUsuario(@RequestBody UsuarioDto usuarioDto) {
+    public ResponseEntity<Object> registrarUsuario(@RequestBody @Validated UsuarioDto usuarioDto) {
         if (this.usuarioService.existsByUsuario(usuarioDto.getUsuario())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Usuário já existe!");
         }
@@ -43,6 +45,22 @@ public class UsuarioController {
     @GetMapping("/{idUsuario}")
     public ResponseEntity<Object> buscarPorId(@PathVariable("idUsuario") UUID idUsuario) {
         return ResponseEntity.ok(this.usuarioService.buscarPorId(idUsuario));
+    }
+
+    @PutMapping("/{idUsuario}")
+    public ResponseEntity<Object> alterarUsuario(@PathVariable UUID idUsuario,
+                                                 @RequestBody @Validated(UsuarioDto.UsuarioView.UsuarioPut.class) UsuarioDto usuarioDto) {
+        Optional<UsuarioModel> usuarioModelOptional = this.usuarioService.buscarPorId(idUsuario);
+        if (usuarioModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado!");
+        }
+        var usuarioModel = usuarioModelOptional.get();
+        usuarioModel.setNome(usuarioDto.getNome());
+        usuarioModel.setEmail(usuarioDto.getEmail());
+        usuarioModel.setTelefone(usuarioDto.getTelefone());
+        usuarioModel.setDataAlteracao(LocalDateTime.now(ZoneId.of("UTC")));
+        this.usuarioService.salvar(usuarioModel);
+        return ResponseEntity.ok(usuarioModel);
     }
 
 }
--- src/main/java/com/estacioneaqui/eausuario/dtos/UsuarioDto.java
+++ src/main/java/com/estacioneaqui/eausuario/dtos/UsuarioDto.java
@@ -1,5 +1,6 @@
 package com.estacioneaqui.eausuario.dtos;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -10,8 +11,13 @@ import javax.validation.constraints.Size;
 @Data
 public class UsuarioDto {
 
+    public interface UsuarioView {
+        interface UsuarioPut {}
+    }
+
     @NotBlank
     @Size(min = 4, max = 150)
+    @JsonView(UsuarioView.UsuarioPut.class)
     private String nome;
 
     @NotBlank
@@ -24,10 +30,13 @@ public class UsuarioDto {
 
     @Email
     @NotBlank
+    @JsonView(UsuarioView.UsuarioPut.class)
     private String email;
 
     @CPF
     private String cpf;
+
+    @JsonView(UsuarioView.UsuarioPut.class)
     private String telefone;
 
 }
