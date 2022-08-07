2df45576eec816965dac1c6c50d5219a68a7bde9
Bruno Alex de Oliveira <brunoalex_88@yahoo.com.br>
2022-08-07 15:37:34 -0300

    Teste de integração de busca de usuário por id #2
    Alteração de checagem

--- src/test/java/com/estacioneaqui/eausuario/controllers/UsuarioControllerRegistrarIntegrationTest.java
+++ src/test/java/com/estacioneaqui/eausuario/controllers/UsuarioControllerRegistrarIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.estacioneaqui.eausuario.controllers;
 
 import com.estacioneaqui.eausuario.dtos.UsuarioDto;
+import com.estacioneaqui.eausuario.models.UsuarioModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -42,11 +43,11 @@ class UsuarioControllerRegistrarIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").isNotEmpty())
-                .andExpect(jsonPath("$.nome").value("Bruno"))
-                .andExpect(jsonPath("$.usuario").value("obruno"))
-                .andExpect(jsonPath("$.email").value("email@email"))
-                .andExpect(jsonPath("$.cpf").value("362.710.578-88"))
-                .andExpect(jsonPath("$.telefone").value("123"))
+                .andExpect(jsonPath("$.nome", equalTo("Bruno")))
+                .andExpect(jsonPath("$.usuario", equalTo("obruno")))
+                .andExpect(jsonPath("$.email", equalTo("email@email")))
+                .andExpect(jsonPath("$.cpf", equalTo("362.710.578-88")))
+                .andExpect(jsonPath("$.telefone", equalTo("123")))
                 .andExpect(jsonPath("$.dataCadastro").isNotEmpty())
                 .andExpect(jsonPath("$.dataAlteracao").isNotEmpty());
 
@@ -76,6 +77,33 @@ class UsuarioControllerRegistrarIntegrationTest {
         mockMvc.perform(get("/usuarios"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    void deveBuscarUmUsuarioPorId() throws Exception {
+        UsuarioDto usuarioDto = UsuarioDto.builder()
+                .nome("Bruno")
+                .usuario("obruno")
+                .senha("123123")
+                .email("email@email")
+                .cpf("362.710.578-88")
+                .telefone("123").build();
+
+        String body = objectMapper.writeValueAsString(usuarioDto);
+
+        String responseBody = mockMvc.perform(post("/usuarios/registrar")
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        UsuarioModel usuarioModel = objectMapper.readValue(responseBody, UsuarioModel.class);
+
+        mockMvc.perform(get("/usuarios/{idUsuario}", usuarioModel.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(usuarioModel.getId())));
+
     }
 
 }
